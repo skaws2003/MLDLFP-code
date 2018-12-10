095dd53bd1edf4bc454fdc16b898dede042d1e44
bkcui <juno@gist.ac.kr>
2018-12-10 15:24:01 +0900

    add batch process

--- code/models/ernn.py
+++ code/models/ernn.py
@@ -9,7 +9,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 class EfficientRNN(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, num_classes=2, num_split=3, device = 'cpu', batch_first = True):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes=2, num_split=3, penalty=0.5, device = 'cpu', batch_first = True):
         super(EfficientRNN, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
@@ -18,9 +18,9 @@ class EfficientRNN(nn.Module):
         self.num_split = num_split
         self.rnns = []    #dim 0 is split, dim 1 is layer
         self.device = device
-        self.layer_weights = torch.nn.Linear(self.hidden_size, self.num_layers)
+        self.layer_weights = torch.nn.Linear(self.hidden_size, self.hidden_size)
         self.batch_first = batch_first
-
+        self.penalty = penalty
 
         for i in range(num_split):
             l = []
@@ -49,50 +49,60 @@ class EfficientRNN(nn.Module):
             h0 = hidden
 
 
-        penalty_layer = torch.ones(max_batch_size, self.num_split) #add penalty layer
+        penalty_layer = torch.ones(1, self.num_split).to(device) #add penalty layer
 
         cur_cell = 0
-        h = torch.zeros(max_batch_size, self.num_layers, self.hidden_size).to(self.device) #hidden initializer
 
-        h[:, 0, :] = self.rnns[cur_cell][0](x[:, 0, :]).view(max_batch_size, 1, h.size(1)) #first layer prop
+        h_c = self.rnns[cur_cell][0](x[:, 0, :]).view(max_batch_size, 1, self.hidden_size) #first layer prop
+        h = h_c
         for i in range(1, self.num_layers):
-            h[:, i, :] = self.rnns[cur_cell][i](h[:, i-1, :]).view(max_batch_size, 1, h.size(1))
+            h_c = self.rnns[cur_cell][i](h[:, i-1, :]).view(max_batch_size, 1, self.hidden_size)
+            h = torch.cat((h, h_c), dim=1)
 
-        outputs = h[:, -1, :].view(max_batch_size, 1, h.size(1))
-        out = h
+
+        penalty_layer[0,cur_cell] = self.penalty*penalty_layer[0,cur_cell]
+        penalty_layer[0] = penalty_layer[0]/penalty_layer.max(1)[0]
+
+        outputs = h_c
+
+        #first prop end
 
         for i in range(1, x.size(1)): #until end of sequence
-            sum_hidden = h
 
+            #finding which cell to use
             if self.num_layers > 1:
                 energy = self.layer_weights(h)
-                layer_energies = torch.sum(hidden * energy, dim=2)
-                sum_hidden = F.softmax(layer_energies, dim=1).unsqueeze(1).bmm(h.transpose(1, 2))
+                layer_energies = torch.sum(energy, dim=2)
+                sum_hidden = F.softmax(layer_energies, dim=1).unsqueeze(1).bmm(h).squeeze(1)
 
 
-            sum_hidden = self.selective_layer(torch.cat((sum_hidden, x[:,i,:].view(max_batch_size, self.input_size)), dim=1)) #select which cell to use on next
+            sum_hidden = self.selective_layer(torch.cat((sum_hidden, x[:,i,:].squeeze(1)), dim=1)) #select which cell to use on next
+
+            ##### need to add penalty layer here
+            sum_hidden = F.softmax(sum_hidden, dim=1)*penalty_layer
 
             sum_hidden = torch.sum(sum_hidden, dim=0) #use sum of all batch
 
-            ##### need to add penalty layer here
+            _, cur_cell = sum_hidden.max(0) #we use maximum mean of batch value of linear layer
 
-            _, cur_cell = sum_hidden.max(1) #we use maximum mean of batch value of linear layer
-
-            h = self.rnns[cur_cell](x[:,i,:])
-
-            h[:, 0, :] = self.rnns[cur_cell][0](x[:, 0, :]).view(max_batch_size, 1, h.size(1))  # first layer prop
+            h_c = self.rnns[cur_cell][0](x[:, 0, :]).view(max_batch_size, 1, self.hidden_size)  # continue prop
+            h = h_c
             for i in range(1, self.num_layers):
-                h[:, i, :] = self.rnns[cur_cell][i](h[:, i - 1, :]).view(max_batch_size, 1, h.size(1))
+                h_c = self.rnns[cur_cell][i](h[:, i-1, :]).view(max_batch_size, 1, self.hidden_size)
+                h = torch.cat((h, h_c), dim=1)
 
-            outputs = torch.cat((outputs, h[:, -1, :]), dim=1) #get last layer as output
-            out = h
+            penalty_layer[0, cur_cell] = self.penalty * penalty_layer[0, cur_cell]
+            penalty_layer[0] = penalty_layer[0] / penalty_layer.max(1)[0]
+
+            outputs = torch.cat((outputs, h_c), dim=1)
 
         #outputs = outputs.transpose(0, 1)
         #out = self.fc(out)
-        return outputs, h.view(max_batch_size, self.hidden_size)
+        return outputs, h
 
 
 def test():
+    batch_size = 4
     input_size = 10
     hidden_size = 20
     seq_length = 5
@@ -100,9 +110,9 @@ def test():
     num_classes = 5
     num_split = 3
     net = EfficientRNN(input_size, hidden_size, num_layers, num_classes, num_split, device=device).to(device)
-    x = torch.randn(4, 5, 10).to(device) # (batch, seq_length, input_size)
-    y = net(x)
-    print(y[0].size())
+    x = torch.randn(batch_size, seq_length, input_size).to(device) # (batch, seq_length, input_size)
+    y, h = net(x)
+    print(y.size())
 
 if __name__=="__main__":
     test()
