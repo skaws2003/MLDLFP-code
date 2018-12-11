aa9f1b2ecd8a82d3c8b4eb825aa4f9c08a2e267c
wkqls0829 <wkqls0829@gmail.com>
2018-12-11 23:32:52 +0900

    stloss

--- code/STLoss.py
+++ code/STLoss.py
@@ -1,11 +1,15 @@
 import torch
 
 class STLoss(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, hyper):
         super(STLoss,self).__init__()
         self.nllloss = torch.nn.NLLLoss()
+        self.hyper = hyper
     
     def forward(self,domain_weight,output,target):
         #domain_weight.shape = [batch_size,seq_len,num_split]
         nll = self.nllloss(output,target)
-        return nll
+        output_loss = nll + (torch.dot(domain_weight, domain_weight) * self.hyper)
+
+
+        return output_loss
